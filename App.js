9fe63304a7b669988c64c455b1f621e76a9f8ac4
Marat Baikin <3371335@gmail.com>
2023-02-22 17:22:22 +0200

    upd

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ImageBackground
+        style={styles.imageBg}
+        source={require("./assets/images/PhotoBG.jpg")}
+      >
+        <View style={styles.formContainer}></View>
+      </ImageBackground>
       <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,7 +20,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
     justifyContent: "center",
+  },
+  formContainer: {
+    // width: 375,
+    height: 549,
+    left: 0,
+    top: 263,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
   },
 });
