8d53c1fc1974b99e44d12fc90938edfc723145e8
Marat Baikin <3371335@gmail.com>
2023-02-22 17:43:34 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
@@ -9,8 +15,9 @@ export default function App() {
         source={require("./assets/images/PhotoBG.jpg")}
       >
         <View style={styles.formContainer}></View>
+        <TextInput style={styles.formInput} />
       </ImageBackground>
-      <Text>Open up App.js to start working on your app!!!</Text>
+      {/* <Text>Open up App.js to start working on your app!!!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -34,5 +41,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
+    justifyContent: "center",
+    // alignItems: "center",
+  },
+  formInput: {
+    // width: 343,
+    height: 50,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    // alignItems: "center",
   },
 });
