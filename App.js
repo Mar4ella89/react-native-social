9d89b067421aeac7e76995b0f12b5590368ee4dc
Marat Baikin <3371335@gmail.com>
2023-02-23 17:17:28 +0200

    upd

--- App.js
+++ App.js
@@ -6,44 +6,51 @@ import {
   ImageBackground,
   TextInput,
   TouchableWithoutFeedback,
+  Keyboard,
+  Button,
 } from "react-native";
 
 export default function App() {
   return (
-    // <TouchableWithoutFeedback onPress={keyboardHide}>
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.imageBg}
-        source={require("./assets/images/PhotoBG.jpg")}
-      >
-        <View style={styles.form}>
-          <Text style={styles.inputTitle}>Регистрация</Text>
-          <View style={styles.textInputeWrapper}>
-            <TextInput
-              style={styles.formInput}
-              textAlign={"left"}
-              placeholder={"Логин"}
-              placeholderTextColor={"#BDBDBD"}
-            />
-            <TextInput
-              style={styles.formInput}
-              textAlign={"left"}
-              placeholder={"Адрес электронной почты"}
-              placeholderTextColor={"#BDBDBD"}
-            />
-            <TextInput
-              style={styles.formInput}
-              textAlign={"left"}
-              placeholder={"Пароль"}
-              placeholderTextColor={"#BDBDBD"}
-            />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.imageBg}
+          source={require("./assets/images/PhotoBG.jpg")}
+        >
+          <View style={styles.formContainer}>
+            <View style={styles.avatarContainer}></View>
+            <Text style={styles.title}>Регистрация</Text>
+            <View style={styles.form}>
+              <View style={styles.textInputeWrapper}>
+                <TextInput
+                  style={styles.formInput}
+                  textAlign={"left"}
+                  placeholder={"Логин"}
+                  placeholderTextColor={"#BDBDBD"}
+                />
+                <TextInput
+                  style={styles.formInput}
+                  textAlign={"left"}
+                  placeholder={"Адрес электронной почты"}
+                  placeholderTextColor={"#BDBDBD"}
+                />
+                <TextInput
+                  style={styles.formInput}
+                  textAlign={"left"}
+                  placeholder={"Пароль"}
+                  placeholderTextColor={"#BDBDBD"}
+                  secureTextEntry={true}
+                />
+              </View>
+              <Button style={styles.button} title="Зарегистрироваться" />
+            </View>
           </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
 
-      <StatusBar style="auto" />
-    </View>
-    // </TouchableWithoutFeedback>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -57,34 +64,39 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
   },
-  form: {
-    // width: 375,
+  formContainer: {
     height: 549,
     left: 0,
     top: 263,
     backgroundColor: "#fff",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    // justifyContent: "center",
-    // alignItems: "center",
   },
-  inputTitle: {
+
+  form: {},
+
+  avatarContainer: {
+    position: "absolute",
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+    top: -60,
+    left: 128,
+  },
+
+  title: {
     fontFamily: "Roboto",
     fontSize: 30,
     fontStyle: "normal",
-    fontWeight: 500,
-    lineHeight: 35,
-    width: 184,
     textAlign: "center",
+    marginTop: 92,
     color: "#212121",
-    // marginTop: 92,
-    // height: 35px;
-    // left: calc(50% - 184px/2 + 0.5px);
-    // top: 0px;
   },
-  // textInputeWrapper: {
-  //   marginTop: 160,
-  // },
+  textInputeWrapper: {
+    marginTop: 32,
+    marginHorizontal: 16,
+  },
   formInput: {
     // width: 343,
     height: 50,
@@ -92,7 +104,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 8,
-    marginHorizontal: 16,
+
     color: "#212121",
     paddingTop: 16,
     paddingLeft: 16,
@@ -101,5 +113,20 @@ const styles = StyleSheet.create({
     marginBottom: 16,
 
     // alignItems: "center",
+  },
+
+  button: {
+    //     display: flex;
+    // flex-direction: column;
+    // align-items: center;
+    // padding: 16px 32px;
+    // gap: 12px;
+    // position: absolute;
+    // height: 51px;
+    // left: 16px;
+    // right: 16px;
+    // bottom: 113px;
+    // background: #FF6C00;
+    // border-radius: 100px;
   },
 });
