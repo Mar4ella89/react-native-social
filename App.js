e39b9bbc487d6aff008fa60a08f23e5489856e21
Marat Baikin <3371335@gmail.com>
2023-03-07 22:22:02 +0200

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <>
       <RegistrationScreen />
-      <LoginScreen />
+      {/* <LoginScreen /> */}
       {/* <StatusBar style="auto" /> */}
     </>
   );
