7910ba4293dba0254dbd1d62b49c83baff44c652
Marat Baikin <3371335@gmail.com>
2023-03-10 01:14:41 +0200

    add main Screen components

---
+++ Screens/mainScreen/CreatePostsScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { StyleSheet, Text, View } from "react-native";
+
+const CreatePostsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>PostsScreen</Text>
+    </View>
+  );
+};
+
+export default CreatePostsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#000",
+  },
+});
---
+++ Screens/mainScreen/ProfileScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { StyleSheet, Text, View } from "react-native";
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>PostsScreen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#000",
+  },
+});
