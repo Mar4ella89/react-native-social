c81112426cf20abc404034b8c0aec7dc54bca374
Marat Baikin <3371335@gmail.com>
2023-03-15 01:32:26 +0200

    Update CreatePostsScreen.jsx

--- Screens/mainScreen/CreatePostsScreen.jsx
+++ Screens/mainScreen/CreatePostsScreen.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
 
 const CreatePostsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>CreatePostsScreen</Text>
+      <Camera style={styles.camera}>
+        <TouchableOpacity style={styles.snapContainer} onPress={() => {}}>
+          <Text style={styles.snapText}>SNAP</Text>
+        </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
@@ -16,8 +20,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    color: "#000",
+  },
+  camera: {
+    height: 300,
+    alignItems: "center",
+    //     position: absolute;
+    // width: 343px;
+    // height: 240px;
+    // left: 16px;
+    // top: 120px;
+    // background: " #F6F6F6",
+    // border: 1px solid #E8E8E8;
+    // border-radius: 8px;
+    // height: 50,
+    backgroundColor: " #F6F6F6",
+    // borderWidth: 1,
+    // borderColor: "#E8E8E8",
+    // borderRadius: 8,
+  },
+  snapText: {
+    color: "#FFFFFF",
+  },
+  snapContainer: {
+    borderWidth: 2,
+    borderColor: "#FFFFFF",
+    borderRadius: 50,
+    marginTop: 230,
+    width: 55,
+    height: 55,
     justifyContent: "center",
     alignItems: "center",
-    color: "#000",
   },
 });
