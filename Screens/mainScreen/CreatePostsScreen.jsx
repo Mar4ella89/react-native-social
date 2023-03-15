49944de6e57fc5f1c531314c55b5bf1dc6bac859
Marat Baikin <3371335@gmail.com>
2023-03-15 11:48:19 +0200

    Update CreatePostsScreen.jsx

--- Screens/mainScreen/CreatePostsScreen.jsx
+++ Screens/mainScreen/CreatePostsScreen.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import { Camera } from "expo-camera";
 
 const CreatePostsScreen = () => {
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState("");
+
+  const takePhoto = async () => {
+    const currentPhoto = await camera.takePictureAsync();
+
+    setPhoto(currentPhoto.uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}>
-        <TouchableOpacity style={styles.snapContainer} onPress={() => {}}>
+      <Camera style={styles.camera} ref={setCamera}>
+        {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image source={{ uri: photo }} style={styles.takePhotoImg} />
+          </View>
+        )}
+
+        <TouchableOpacity style={styles.snapContainer} onPress={takePhoto}>
           <Text style={styles.snapText}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
@@ -19,12 +34,13 @@ export default CreatePostsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    color: "#000",
+    // backgroundColor: "#fff",
+    // color: "#000",
   },
   camera: {
-    height: 300,
+    height: "45%",
     alignItems: "center",
+    justifyContent: "flex-end",
     //     position: absolute;
     // width: 343px;
     // height: 240px;
@@ -34,7 +50,7 @@ const styles = StyleSheet.create({
     // border: 1px solid #E8E8E8;
     // border-radius: 8px;
     // height: 50,
-    backgroundColor: " #F6F6F6",
+    // backgroundColor: " #F6F6F6",
     // borderWidth: 1,
     // borderColor: "#E8E8E8",
     // borderRadius: 8,
@@ -46,10 +62,21 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "#FFFFFF",
     borderRadius: 50,
-    marginTop: 230,
+    marginBottom: 30,
     width: 55,
     height: 55,
     justifyContent: "center",
     alignItems: "center",
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    borderColor: "#FFF",
+    borderWidth: 1,
+  },
+  takePhotoImg: {
+    height: 200,
+    width: 200,
   },
 });
